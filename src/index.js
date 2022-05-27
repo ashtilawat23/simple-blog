ceda24dbffcf07228507e9b51a8e5128a477ef1f
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-27 12:21:20 -0400

    chakra working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { extendTheme, ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ config });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider resetCSS={true} theme={theme}>
+    <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
