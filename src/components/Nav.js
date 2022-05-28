74845e771bc4706d89d49b8a09c9307d20b8eb56
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-28 18:24:54 -0400

    dropdown added to common

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,16 @@
 import React from 'react';
-import {
-  Box,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button,
-} from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Dropdown } from './common/Dropdown';
+import { Box } from '@chakra-ui/react';
 
-const jobSeekers = ['Search Jobs', 'Search Salaries', 'Create Free Account'];
+const jobSeekers = {title: 'Job Seekers', items: ['Search Jobs', 'Search Salaries', 'Create Free Account']};
 
-const businesses = ['Post a Job', 'How it Works', 'Employer Support'];
+const businesses = {title: 'Businesses', items: ['Post a Job', 'How it Works', 'Employer Support']};
 
 export const Nav = () => {
   return (
     <Box className='nav'>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-          Job Seekers
-        </MenuButton>
-        <MenuList>
-            {jobSeekers.map((action) => 
-                <MenuItem key={action}>{action}</MenuItem>
-            )}
-        </MenuList>
-      </Menu>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-          Businesses
-        </MenuButton>
-        <MenuList>
-            {businesses.map((action) => 
-                <MenuItem key={action}>{action}</MenuItem>
-            )}
-        </MenuList>
-      </Menu>
+        <Dropdown title={jobSeekers.title} items={jobSeekers.items} />
+        <Dropdown title={businesses.title} items={businesses.items} />
     </Box>
   );
 };
