acaf75326c84ef230347450c9837de1ef99bb799
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-29 00:27:47 -0400

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+export const Footer = () => {
+  return (
+    <Box className='footer'>
+        <Text fontFamily={'Space Mono'}>
+            BOOTCAMP1ST, Inc.&copy; 2022
+        </Text>
+        <Text fontFamily={'Space Mono'}>
+            All Rights Reserved
+        </Text>
+    </Box>
+  )
+}
