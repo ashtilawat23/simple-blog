b1f10b163a6f0aba4cef375ef8e91166a119f951
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-28 23:18:54 -0400

    hamburger added & made header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Button, useMediaQuery } from '@chakra-ui/react';
+import { Hamburger } from './Hamburger';
 import { Nav } from './Nav';
 import { QuickLinks } from './QuickLinks';
 
 export const Header = () => {
-  return (
-    <Box className="header">
-      <Nav />
-      <QuickLinks />
-    </Box>
-  );
+  const [isLargerThan1200] = useMediaQuery('(min-width: 1200px)');
+  if (isLargerThan1200) {
+    return (
+      <Box className="header">
+        <Nav />
+        <QuickLinks />
+      </Box>
+    );
+  } else {
+    return (
+      <Box className="hamburgerHeader">
+        <Box className="hamburgerLeft">
+          <Heading as="em">BOOTCAMP1ST</Heading>
+          <Button>Post a Job</Button>
+        </Box>
+        <Hamburger />
+      </Box>
+    );
+  }
 };
