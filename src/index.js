b7ca433adb8cc806ab19528068513b9e5a511762
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-01 22:37:34 -0400

    auth0 working

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    clientId={process.env.REACT_APP_CLIENT_ID}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <ChakraProvider resetCSS={true} theme={theme}>
